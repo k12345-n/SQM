b9610b94137f4af5744343df3a855ce0e1675bf8
Ken <khorkenjoo@gmail.com>
2026-05-07 14:42:09 +0800

    x

---
+++ src/main/java/test/sqm/App.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package test.sqm;
+
+import ucpbased.UCP;
+
+public class App {
+    public static void main(String[] args) {
+        UCP ucp = new UCP();
+
+        ucp.input_UUCW(0, 29, 0);
+        ucp.input_UAW(0, 0, 5);
+        ucp.input_TCF(3, 3, 3, 3, 0, 3, 3, 0, 3, 3, 3, 0, 3);
+        ucp.input_ECF(3, 3, 3, 3, 3, 3, 0, 0);
+
+        System.out.println(ucp.getUCP());
+        System.out.println(ucp.getPF());
+    }
+}
